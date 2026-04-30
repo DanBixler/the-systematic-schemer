16cd752b522a36c42a0e8e20925454e636fab049
DanBixler <dan.bixler007@gmail.com>
2026-04-29 22:12:08 -0400

    Organize repo into core/ and chapters/ and add lat? to Ch 2

---
+++ chapters/ch02-do-it-again/lat.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require rackunit)
+(require rackunit/text-ui)
+(require "../../core/atom.rkt")
+
+;; lat - list of atoms
+
+;; ==========================================================
+;; Data Definitions
+
+;; A List-of-Atoms (LAT) is one of:
+;;  - '()
+;;  - (cons Atom LAT)
+;; interp. a list where every element is a single, indivisible value
+
+;; ==========================================================
+;; Functions
+
+;; lat? : LAT -> Boolean
+;; produce #t if every element in the list is an atom; #f otherwise
+
+
+#|---- Template ------
+ (define (fn-for-lat l)
+   (cond [(null? l) (...)]                ; Base Case: The list is empty
+         [else
+          (... (car l)                    ; The first element (an Atom)
+               (fn-for-lat (cdr l)))]))   ; Natural Recursion on the rest
+
+ LAT -> Boolean
+ produce #t if every element in the list is an atom; #f otherwise
+|#
+
+
+(define (lat? l)
+  (cond [(null? l) #t]
+        [(atom? (car l)) (lat? (cdr l))]        ; If car is an atom, check the rest
+        [else #f]))                             ; If car isn't an atom, it's not a LAT
+
+
+;; --- Tests (using Rackunit for #lang racket) ---
+
+(module+ test
+  (test-case "Testing lat?"
+    (check-true  (lat? '(bacon lettuce tomato)))
+    (check-false (lat? '(bacon (lettuce) tomato)))
+    (check-true  (lat? '()))))
